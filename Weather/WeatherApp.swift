6b609aeb7936544552c0ddd119d194a9ef22b08e
Sayaka <sayaka.ssk30@gmail.com>
2025-11-09 08:34:32 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sayaka Sasaki on 2025/11/09.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
